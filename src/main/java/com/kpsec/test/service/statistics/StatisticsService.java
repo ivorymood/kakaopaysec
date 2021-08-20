69dc9c05474764900ce203d63ea4d58e3a0c1d1f
hylim <ivorymood@gmail.com>
2021-08-21 00:03:39 +0900

    ADD : Q2) 2018,2019 거래 없는 고객 추출 api

--- src/main/java/com/kpsec/test/service/statistics/StatisticsService.java
+++ src/main/java/com/kpsec/test/service/statistics/StatisticsService.java
@@ -1,7 +1,11 @@
 package com.kpsec.test.service.statistics;
 
+import com.kpsec.test.domain.entity.Account;
+import com.kpsec.test.domain.entity.Statistics;
+import com.kpsec.test.repository.account.AccountRepository;
 import com.kpsec.test.repository.statistics.StatisticsRepository;
 import com.kpsec.test.service.statistics.dto.YearDTO;
+import com.kpsec.test.vo.YearlyNonTransactionAccountVO;
 import com.kpsec.test.vo.StatisticsInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,9 @@ import java.util.List;
 public class StatisticsService {
 
     @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
     private StatisticsRepository statisticsRepository;
 
     public List<StatisticsInterface> getYearlyTopAmountAccounts(List<YearDTO> dtoList) {
@@ -25,5 +32,32 @@ public class StatisticsService {
         }
 
         return statisticsRepository.findAllYearlyAmountAccounts(yearList);
+    }
+
+    public List<YearlyNonTransactionAccountVO> getYearlyNonTransactionAccounts(List<YearDTO> dtoList) {
+
+        List<String> yearList = new ArrayList<>();
+        for (YearDTO dto : dtoList) {
+            yearList.add(dto.getYear());
+        }
+
+        List<Statistics> statisticsList = statisticsRepository.findAllByYearIsInOrderByYear(yearList);
+        List<Account> accountList = accountRepository.findAll();
+
+        List<YearlyNonTransactionAccountVO> resultList = new ArrayList<>();
+        for (String year : yearList) {
+            for (Account account : accountList) {
+
+                long count = statisticsList.stream().parallel()
+                        .filter(s -> year.equals(s.getYear())
+                                && account.getAccountNo().equals(s.getAccount().getAccountNo()))
+                        .count();
+
+                if (count == 0) {
+                    resultList.add(new YearlyNonTransactionAccountVO(year, account.getAccountName(), account.getAccountNo()));
+                }
+            }
+        }
+        return resultList;
     }
 }
---
+++ src/main/java/com/kpsec/test/vo/YearlyNonTransactionAccountVO.java
@@ -0,0 +1,15 @@
+package com.kpsec.test.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class YearlyNonTransactionAccountVO {
+
+    private String year;
+    private String name;
+    private String acctNo;
+}

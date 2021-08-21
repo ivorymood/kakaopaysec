22417ceeb5afae4a2c601146fac42b34aa613b29
hylim <ivorymood@gmail.com>
2021-08-21 23:43:50 +0900

    FIX : 환경설정, 테스트클래스명 변경

---
+++ src/test/java/com/kpsec/test/contoller/StatisticsControllerTest.java
@@ -0,0 +1,30 @@
+package com.kpsec.test.contoller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsControllerTest {
+
+    @BeforeEach
+    void createDocument() {
+
+    }
+
+    @Test
+    void getYearlyTopAmountAccounts() {
+    }
+
+    @Test
+    void getYearlyNonTransactionAccounts() {
+    }
+
+    @Test
+    void getYearlyAmountSumByBranch() {
+    }
+
+    @Test
+    void getAmountSumByBranch() {
+    }
+}

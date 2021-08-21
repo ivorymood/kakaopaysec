5cdd0a7e65e8166352788ce049bdf54bf786e73a
hylim <ivorymood@gmail.com>
2021-08-22 08:49:16 +0900

    FIX : 불필요한 어노테이션 정리

--- src/main/java/com/kpsec/test/service/statistics/dto/YearDTO.java
+++ src/main/java/com/kpsec/test/service/statistics/dto/YearDTO.java
@@ -2,14 +2,12 @@ package com.kpsec.test.service.statistics.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class YearDTO {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy", timezone = "Asia/Seoul")
-    String year;
+    private String year;
 }
